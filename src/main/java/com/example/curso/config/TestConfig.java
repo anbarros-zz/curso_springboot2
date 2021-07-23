f6095e311c64245d65b4072e524cab699e80eaf4
Anderson Barros <Anderson Barros>
2021-07-23 19:37:42 -0300

    Payment, one-to-one association

--- src/main/java/com/example/curso/config/TestConfig.java
+++ src/main/java/com/example/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.curso.entity.Category;
 import com.example.curso.entity.Order;
 import com.example.curso.entity.OrderItem;
+import com.example.curso.entity.Payment;
 import com.example.curso.entity.Product;
 import com.example.curso.entity.User;
 import com.example.curso.entity.enuns.OrderStatus;
@@ -81,7 +82,9 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
-		
+		Payment pay1 = new Payment(null ,Instant.parse("2019-06-20T21:53:07Z"),o1 );
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 
 }
